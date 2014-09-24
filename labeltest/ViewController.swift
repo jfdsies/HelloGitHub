656a54922ae773313ff84a26f5d2f2540e249474
jfdsies <sora@soras-Mac.local>
2014-09-23 23:29:14 -0700

    hello~world 已删除～添加label测试

--- labeltest/ViewController.swift
+++ labeltest/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelBody: UITextView!
+    @IBOutlet weak var headTitle: UILabel!
+    @IBOutlet weak var outlineButton: UIButton!
 
+    @IBAction func changeBodySelectionColorToMatchBackgroundOfButton(sender: UIButton) {
+        labelBody.textStorage.addAttribute(NSForegroundColorAttributeName, value: sender.backgroundColor!, range: labelBody.selectedRange)
+    }
+    
+    @IBAction func outlineBodySelectionText() {
+        labelBody.textStorage.addAttributes(
+            [NSStrokeWidthAttributeName: -3,
+             NSStrokeColorAttributeName: UIColor.blackColor()
+            ], range: labelBody.selectedRange)
+    }
+    
+    @IBAction func unoutlineBodySelectionText() {
+        labelBody.textStorage.removeAttribute(NSStrokeWidthAttributeName, range: labelBody.selectedRange)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var title:NSMutableAttributedString = NSMutableAttributedString(string: outlineButton.currentTitle!)
+        title.setAttributes([
+            NSStrokeWidthAttributeName: 3,
+            NSStrokeColorAttributeName: outlineButton.tintColor!], range: NSMakeRange(0, title.length))
+        outlineButton.setAttributedTitle(title, forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
