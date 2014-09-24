959f49d43ec5099778c9c8916ac75460e326faa0
jfdsies <sora@soras-Mac.local>
2014-09-23 17:50:03 -0700

    Initial Commit

---
+++ labeltest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  labeltest
+//
+//  Created by sora on 23/9/14.
+//  Copyright (c) 2014年 sora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
